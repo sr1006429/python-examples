d402018f2172dc09bf399111078a4a0025fb88a7
Ruslan <sr1006429@gmail.com>
2023-06-10 18:16:48 +0300

    update

---
+++ speak/main.py
@@ -0,0 +1,6 @@
+﻿# пример https://pypi.org/project/gTTS/
+# есть еще один модуль - pyttsx3 для преобразования текста в речь 
+
+from gtts import gTTS
+tts = gTTS('Добрый день. Это сообщение озвучивается и записывается в файл hello.mp3', lang='ru')
+tts.save('hello.mp3')
---
+++ translate/main.py
@@ -0,0 +1,22 @@
+﻿# pip install googletrans
+# or if the problem is in the code
+#       pip uninstall googletrans
+#       pip install googletrans==4.0.0rc1 
+
+from googletrans import Translator
+
+
+translator = Translator()
+
+def translator_translate(word):
+    result = translator.translate(word, src = 'ru', dest = 'en')
+    return result.text    
+
+word = 'Здравствуйте. Это проверка перевода с русского языка на английский'
+text = translator_translate(word)
+
+print(word)
+print('перевод :')
+print(text)
+
+
